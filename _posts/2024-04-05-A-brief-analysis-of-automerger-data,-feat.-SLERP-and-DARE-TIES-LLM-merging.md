e83b040486004390ba1c1234dd6b26fc4cdcb237
Kostis Gourgoulias <kgourgoulias@gmail.com>
2024-04-05 15:16:34 +0100

    new post

---
+++ _posts/2024-04-05-A-brief-analysis-of-automerger-data,-feat.-SLERP-and-DARE-TIES-LLM-merging.md
@@ -0,0 +1,10 @@
+---
+layout: distill
+title: "A brief analysis of automerger data, feat. SLERP and DARE-TIES LLM merging"
+date: 2024-04-05 15-14-54 +0100
+category: 
+tags: 
+---
+
+[An article I wrote](https://huggingface.co/blog/kgourgou/a-first-look-at-automerger-data) as a community blog post at HuggingFace.
+
---
+++ new-post.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 KG
+
+"""
+
+$ python new-post "title of the post"
+
+new-post can also be executed directly:
+
+$ new-post "title of the post"
+
+"""
+
+import argparse
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Jekyll Post")
+    parser.add_argument("title", help="The title of the post.")
+    # parser.add_argument('category', help='The categores of the post.')
+    parser.add_argument("--tag", help="The tags of the post.", default="blog")
+    args = parser.parse_args()
+
+    title = args.title
+    categories = ""  # args.category
+    tags = ""  # args.tag if args.tag else args.category
+
+    name, date = (
+        time.strftime("%Y-%m-%d") + "-" + title.replace(" ", "-") + ".md",
+        time.strftime("%Y-%m-%d %H-%M-%S %z"),
+    )
+
+    path = "_posts/" + name
+
+    yaml = """
+---
+layout: distill
+title: "%s"
+date: %s
+category: %s
+tags: %s
+---"""
+    post = yaml % (title, date, categories, tags)
+    fp = None
+    try:
+        fp = open(path, "w")
+        fp.write(post)
+        print("It all worked out!")
+    finally:
+        if fp:
+            fp.close()
+
+
+if __name__ == "__main__":
+    main()
